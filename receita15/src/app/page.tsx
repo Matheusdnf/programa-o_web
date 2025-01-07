09751bb725888366ba95e9330db5d3c63096c9b6
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-06 21:39:34 -0300

    feat - requisição api

--- receita15/src/app/page.tsx
+++ receita15/src/app/page.tsx
@@ -1,9 +1,32 @@
 import Link from "next/link";
+interface Movie {
+  Title: string;
+  Year: String;
+  // S - tipo de objeto
+  // s - tipo primitivo, o que deve ser utilizado na key
+  imdbID: string;
+  titleSearchKey: "";
+}
+const transformar = (vetor: Movie) => (
+  <div key={vetor.imdbID}>
+    - {vetor.Title} | {vetor.Year} | --- {vetor.imdbID}
+  </div>
+);
 
+async function requisicao(busca: String) {
+  const titleSearchKey = busca || "bagdad";
+  const res = await fetch(
+    `http://www.omdbapi.com/?i=tt3896198&apikey=9eabd38d&s=${titleSearchKey}`
+  );
+
+  const data = await res.json();
+  return <div>{data.Search.map(transformar)}</div>;
+}
 export default function Home() {
   return (
     <div>
       <div>
+        <div>{requisicao("avengers")}</div>
         <h1>Viva Santana!</h1>
         {/* Através disso vc só carregará o que for importante */}
         {/* Forma nova utilizando do link fornecido pelo react */}
