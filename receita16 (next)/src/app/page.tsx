dd9c751d4b07c557fdecd25a06ccf4c9bf97b71c
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-16 15:03:53 -0300

    feat - requisição com api

--- receita16 (next)/src/app/page.tsx
+++ receita16 (next)/src/app/page.tsx
@@ -1,46 +1,9 @@
 import Link from "next/link";
-import Image from "next/image";
 
-interface Movie {
-  Title: string;
-  Year: String;
-  Poster: String;
-  // S - tipo de objeto
-  // s - tipo primitivo, o que deve ser utilizado na key
-  imdbID: string;
-  titleSearchKey: "";
-}
-const transformar = (vetor: Movie) => (
-  <div key={vetor.imdbID}>
-    - {vetor.Title} | {vetor.Year} | --- {vetor.imdbID}
-    <div>
-      <Image
-        src={vetor.Poster}
-        alt={`Poster do filme ${vetor.Title}`}
-        width={300}
-        height={300}
-        placeholder="blur"
-        blurDataURL="/placeholder.png"
-      />
-    </div>
-  </div>
-);
-
-async function requisicao(busca: String, tipo: string) {
-  const tipoSearchky = tipo || "movie";
-  const titleSearchKey = busca || "bagdad";
-  const res = await fetch(
-    `http://www.omdbapi.com/?i=tt3896198&apikey=9eabd38d&s=${titleSearchKey}&type=${tipoSearchky}`
-  );
-
-  const data = await res.json();
-  return <div>{data.Search.map(transformar)}</div>;
-}
 export default function Home() {
   return (
     <div>
       <div>
-        <div>{requisicao("avengers", "series")}</div>
         <h1>Viva Santana!</h1>
         {/* Através disso vc só carregará o que for importante */}
         {/* Forma nova utilizando do link fornecido pelo react */}
