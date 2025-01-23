9cc1312e9567ddc11911eec34010d9553bac16f1
Matheusdnf <matheusdiniz870@gmail.com>
2025-01-23 20:03:24 -0300

    feat - configuração do nextconfig

--- receita15 (next)/next.config.ts
+++ receita15 (next)/next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["m.media-amazon.com"], // Adicione o domínio aqui
+    domains: ["m.media-amazon.com"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "www.omdbapi.com",
+        pathname: "/**",
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: "http",
-      hostname: "www.omdbapi.com",
-      pathname: "/**",
-    },
-  ],
 };
 
 export default nextConfig;
